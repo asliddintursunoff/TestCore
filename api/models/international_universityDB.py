4626d3b4588795222caa08327736352955b3961e
asliddintursunoff <asliddintursunoff@icloud.com>
2025-07-12 22:39:24 +0500

    modify db

--- api/models/international_universityDB.py
+++ api/models/international_universityDB.py
@@ -11,8 +11,13 @@ class UniversityDB(models.Model):
         verbose_name = "Xalqaro Universitet"
         verbose_name_plural = "Xalqaro Universitetlar"
     
+    def __str__(self):
+        return f"ID: {self.id} -  name: {self.university_name}"
 class FacultyDB(models.Model):
     university = models.ForeignKey(UniversityDB,on_delete=models.CASCADE, related_name="faculties")
     faculty_name = models.CharField(max_length=200)
     description = models.TextField()
+
+    def __str__(self):
+        return f"ID: {self.id} -  name: {self.faculty_name}"
 
