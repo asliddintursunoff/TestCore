b5d551bfffc674316129623d3930f0eb9e780dd2
asliddintursunoff <asliddintursunoff@icloud.com>
2025-07-21 16:36:47 +0500

    new

--- api/models/international_universityDB.py
+++ api/models/international_universityDB.py
@@ -5,7 +5,16 @@ from django.db import models
 class UniversityDB(models.Model):
     university_name = models.CharField(max_length=200)
     university_short_name = models.CharField(max_length=200)
-    university_picture = models.ImageField(upload_to="media/universities")
+    description = models.TextField(default=" ")
+    university_logo_picture = models.ImageField(upload_to="media/universities")
+    university_img = models.ImageField(upload_to="media/universities",null=True,blank=True)
+    contract_sum_start = models.IntegerField(default=0)
+    contract_sum_finish = models.IntegerField(default=0)
+    phone_number = models.CharField(max_length=13)
+    location = models.TextField(default="Tashkent")
+    instagram_link = models.CharField(max_length=200,null=True,blank=True)
+    telegram_link = models.CharField(max_length=200,null=True,blank=True)
+    website_link = models.CharField(max_length=200,null=True,blank=True)
 
     class Meta:
         verbose_name = "Xalqaro Universitet"
